2f2f4020f44690bfbc7585dea1b37ebb4b296252
yujinpan <1192878390@qq.com>
2018-12-24 10:38:58 +0800

    build: 不清空public目录

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,4 +1,4 @@
 import * as shell from "shelljs";
 
-shell.rm("-rf", "dist/public");
-shell.cp("-R", "public", "dist/public");
+// shell.rm("-rf", "dist/public");
+shell.cp("-R", "public", "dist/");
