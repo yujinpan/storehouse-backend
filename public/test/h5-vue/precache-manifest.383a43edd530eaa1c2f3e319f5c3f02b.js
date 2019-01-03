4cd93c474f5b753ac5e0e54ceab65d80b7bb0354
yujinpan <1192878390@qq.com>
2019-01-03 09:40:39 +0800

    feat: 增加前端测试目录(pwa测试)

---
+++ public/test/h5-vue/precache-manifest.383a43edd530eaa1c2f3e319f5c3f02b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/h5-vue/robots.txt"
+  },
+  {
+    "revision": "6e695a1414a3f77cd9b5",
+    "url": "/h5-vue/js/example.7a6a0b49.js"
+  },
+  {
+    "revision": "df34afabee906d65c410",
+    "url": "/h5-vue/js/example-router.4f8d53da.js"
+  },
+  {
+    "revision": "dbae6274dc526d5a87e1",
+    "url": "/h5-vue/js/chunk-vendors.8c799401.js"
+  },
+  {
+    "revision": "26e76b42468b194365b9f6bba42de8d0",
+    "url": "/h5-vue/example.html"
+  },
+  {
+    "revision": "f5f9fe633c2f70e397eac536ab80d708",
+    "url": "/h5-vue/example-router.html"
+  },
+  {
+    "revision": "6e695a1414a3f77cd9b5",
+    "url": "/h5-vue/css/example.9b53f944.css"
+  },
+  {
+    "revision": "df34afabee906d65c410",
+    "url": "/h5-vue/css/example-router.4de9a6fb.css"
+  }
+];
