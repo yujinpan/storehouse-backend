a92b3a3fb208b2394fc5415f1fce92cdd3ead9d9
yujinpan <1192878390@qq.com>
2019-01-03 10:26:05 +0800

    chore: 更新前端资源

--- public/test/h5-vue/precache-manifest.0aadb3890c7b0d55fef99d2e55dad21a.js
+++ public/test/h5-vue/precache-manifest.0aadb3890c7b0d55fef99d2e55dad21a.js
@@ -4,31 +4,31 @@ self.__precacheManifest = [
     "url": "/test/h5-vue/robots.txt"
   },
   {
-    "revision": "00b3f81d5661085ab4cb",
-    "url": "/test/h5-vue/js/example.dc103e09.js"
+    "revision": "46e8e584321b696719a5",
+    "url": "/test/h5-vue/js/example.1fd32b6c.js"
   },
   {
-    "revision": "ef07b55ca33148d73e04",
-    "url": "/test/h5-vue/js/example-router.fcf6842f.js"
+    "revision": "eb8ad5351035d675a1fd",
+    "url": "/test/h5-vue/js/example-router.242c10b5.js"
   },
   {
     "revision": "dbae6274dc526d5a87e1",
     "url": "/test/h5-vue/js/chunk-vendors.8c799401.js"
   },
   {
-    "revision": "673890a19c26e1bccd9d1ca4ae825c78",
+    "revision": "908bbe49073a02963c65ad14b9a1695c",
     "url": "/test/h5-vue/example.html"
   },
   {
-    "revision": "a02c96da023a45e22777b087e5f105c0",
+    "revision": "ef201797fef8b3d64dfeb706bf16c3f4",
     "url": "/test/h5-vue/example-router.html"
   },
   {
-    "revision": "00b3f81d5661085ab4cb",
+    "revision": "46e8e584321b696719a5",
     "url": "/test/h5-vue/css/example.9b53f944.css"
   },
   {
-    "revision": "ef07b55ca33148d73e04",
+    "revision": "eb8ad5351035d675a1fd",
     "url": "/test/h5-vue/css/example-router.4de9a6fb.css"
   }
 ];
